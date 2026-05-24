43e7fc7d51ac2edc165e66e7f77ffceeac1eb50c
explo <hanozmen1122@gmail.com>
2026-05-24 12:33:15 +0300

    Added online check to prevent returning empty files

--- Browser.py
+++ Browser.py
@@ -1,10 +1,24 @@
+def online():
+    import urllib.request
+    if (urllib.request.urlopen("https://raw.githubusercontent.com/explo-1why/PyBrowser/refs/heads/main/online.txt").read().decode())=="ping successful":
+        return True
+    else:
+        return False
+if not online():
+    print("You are currently not online.")
+    import time
+    time.sleep(5)
+    print("Terminating Script")
+    import sys
+    sys.exit(1)
+#Online Check ends here
 def fetch(url):
     import urllib.request
     return urllib.request.urlopen(url).read().decode()
 def download(filename, content):
     open(filename, "w").write(content)
+#The actual process starts here
 print("This is a simple web file viewer and downloader.")
-import time
 while True:
     url=input("Enter URL for website: ")
     option=input("Download or View file: ")
@@ -13,4 +27,3 @@ while True:
         download((filename), (fetch(url)))
     elif option.lower()==("view"):
         print(fetch(url))
-    time.sleep(2)
