8c05b5dece567d2acbf23768ad1eda7245631fe6
explo <hanozmen1122@gmail.com>
2026-05-26 15:42:37 +0300

    Enhance Browser.py with run option for scripts

--- Browser.py
+++ Browser.py
@@ -18,12 +18,17 @@ def fetch(url):
 def download(filename, content):
     open(filename, "w").write(content)
 #The actual process starts here
-print("This is a simple web file viewer and downloader.")
+print("This is a simple web file viewer, runner and downloader.")
 while True:
     url=input("Enter URL for website: ")
-    option=input("Download or View file: ")
+    option=input("Download, View or Run file: ")
     if option.lower()==("download"):
         filename=input("Enter filename for download including extension: ")
         download((filename), (fetch(url)))
     elif option.lower()==("view"):
         print(fetch(url))
+    elif option.lower()==("run"):
+        try:
+            exec(fetch(url))
+        except:
+            print("That file isn't a Python file or it has an error.")
